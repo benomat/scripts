c1d49b31df5f83b5479a3cf19b4971a20063f1b9
benomat <ebeling.pro@gmail.com>
2024-07-01 01:56:11 +0200

    sobbing

--- myown/wsBoost.lua
+++ myown/wsBoost.lua
@@ -1,10 +1,16 @@
 return function(s)
     _G.cframespeed=s
-    if not _G.wsbran then 
-    _G.wsbran=true
-        while _G.wsbran do 
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame + game.Players.LocalPlayer.Character.Humanoid.MoveDirection * _G.cframespeed
+    if not _G.cframeran then 
+    _G.cframeran=true
+        while true do
+            pcall( function()
+            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame + game.Players.LocalPlayer.Character.Humanoid.MoveDirection * _G.cframespeed end)
             game:GetService("RunService").Stepped:wait()
+            if _G.cframespeed==0 then
+                _G.cframeran = false
+                print("YURR")
+                break
+            end
         end
     end
 end
