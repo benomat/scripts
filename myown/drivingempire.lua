e76cf1f2f5c8cb0ad5d0aa4be454dd131154283b
benomat <ebeling.pro@gmail.com>
2024-07-01 00:08:45 +0200

    driving empire update

--- myown/drivingempire.lua
+++ myown/drivingempire.lua
@@ -55,7 +55,7 @@ emoteSelector=tab:CreateDropdown(
 end)
 tab:CreateButton("Reset Emote Selector",function()emoteSelector:Set({})end)
 tab:CreateLabel("Press the PERIOD key . to use emotes")
-MISCTAB:CreateSlider("Speed",{16, 300},1,"studs per second",game.Players.LocalPlayer.Character.Humanoid.WalkSpeed,function(v) game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = v end)
+MISCTAB:CreateSlider("Walk Speed",{16, 300},1,"studs per second",game.Players.LocalPlayer.Character.Humanoid.WalkSpeed,function(v) game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = v end)
 -- wsBoost=loadstring(game:HttpGet("https://raw.githubusercontent.com/benomat/scripts/m/myown/wsBoost.lua"))()
 -- MISCTAB:CreateSlider("CFrame Speed",{0, 150},1,"boost",0,function(v) wsBoost(v/25) end)
 MISCTAB:CreateButton("Rejoin Server", function () loadstring(game:HttpGet('https://pastebin.com/raw/e8jN1Lvu'))() end)
