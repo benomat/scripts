1f514b5633a73496492cf27d84f13b903d07eb66
benomat <ebeling.pro@gmail.com>
2024-07-02 16:34:32 +0200

    wwww

--- zzTab2.lua
+++ zzTab2.lua
@@ -1,1 +1,1 @@
-setclipboard("elseif (p=="..game.PlaceId.." or  g=="..game.GameId..") then --"..game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name)
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Kiroftt/Survive-The-Killer/main/Simple'))()
