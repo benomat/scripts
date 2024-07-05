9bf178299498cb32a484496213694832a7be4b76
benomat <ebeling.pro@gmail.com>
2024-07-05 09:19:44 +0200

    counterblox update

--- zzTab.lua
+++ zzTab.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://gist.githubusercontent.com/afyzone/d8b7c8da9fb09c80937f4536648dbd9a/raw/'))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ik3ac/stormy-fixed-counter-blox-2023/main/.gitignore"))()
--- zzzzzz.lua
+++
@@ -1,1 +0,0 @@
-setclipboard("elseif (p=="..game.PlaceId.." or  g=="..game.GameId..") then --"..game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name)
